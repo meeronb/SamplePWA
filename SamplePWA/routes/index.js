21f6ca52a69212247cc8a96836cfb38d2df073f3
Pratik Khandelwal <prkhandelwal@hotmail.com>
2018-05-10 19:08:26 +0530

    Add project files.

---
+++ SamplePWA/routes/index.js
@@ -0,0 +1,14 @@
+﻿'use strict';
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function (req, res) {
+    res.render('index', { title: 'Express' });
+});
+
+router.get('/offline', function (req, res) {
+    res.sendFile('public/Offline.html');
+});
+
+module.exports = router;
