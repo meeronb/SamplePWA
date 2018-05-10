9ba53c6778d630e52e1f060e808db009134165ea
Pratik Khandelwal <prkhandelwal@hotmail.com>
2018-05-10 19:17:59 +0530

    Handling push related server requests

--- SamplePWA/routes/index.js
+++ SamplePWA/routes/index.js
@@ -24,5 +24,29 @@ webpush.setVapidDetails(
     vapidKeys.privateKey
 );
 
+router.get('/vapidPublicKey', function (req, res) {
+    res.send(vapidKeys.publicKey);
+});
+
+router.post('/register', function (req, res) {
+    // A real world application would store the subscription info.
+    res.sendStatus(201);
+});
+
+router.post('/sendNotification', function (req, res) {
+    const subscription = req.body.subscription;
+    const payload = 'payload';
+    const options = null;
+
+    webpush.sendNotification(subscription, payload, options)
+        .then(function () {
+            res.sendStatus(201);
+        })
+        .catch(function (error) {
+            res.sendStatus(500);
+            console.log(error);
+        });
+});
+
 
 module.exports = router;
