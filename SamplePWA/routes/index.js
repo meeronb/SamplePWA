d3ba87e03ef104ec378ca89935c9510b5883c751
Pratik Khandelwal <prkhandelwal@hotmail.com>
2018-05-10 19:10:58 +0530

    Added web-push

--- SamplePWA/routes/index.js
+++ SamplePWA/routes/index.js
@@ -2,6 +2,7 @@
 var express = require('express');
 var router = express.Router();
 
+var webpush = require('web-push');
 /* GET home page. */
 router.get('/', function (req, res) {
     res.render('index', { title: 'Express' });
